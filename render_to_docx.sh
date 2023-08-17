a1e7ff1bd9ac99ab7c0ef3b4a24de9e957d8c3f2
Alexey Medvedev <alexey.medvedev@bsc.es>
2023-08-17 17:02:48 +0200

    Changes in styling: margin parameter now in style files (no more a field MARGIN in source); more parameters in style files; field LANGUAGE in source renamed to STYLE.

--- render_to_docx.sh
+++ render_to_docx.sh
@@ -16,19 +16,14 @@ author_field=$(grep '\[meta_author\]:.*AUTHOR:' "$1")
 [ -z "$author_field" ] && fatal "AUTHOR field must be present in MD file as a comment."
 AUTHOR=$(echo "$author_field" | sed 's/^\[meta_author\]:.*AUTHOR: //')
 
-lang_field=$(grep '\[meta_language\]:.*LANGUAGE:' "$1")
-[ -z "$lang_field" ] && fatal "LANGUAGE field must be present in MD file as a comment."
-LANGUAGE=$(echo "$lang_field" | sed 's/^\[meta_language\]:.*LANGUAGE: //')
-
-margin_field=$(grep '\[meta_margin\]:.*MARGIN:' "$1")
-[ -z "$margin_field" ] && fatal "MARGIN field must be present in MD file as a comment."
-MARGIN=$(echo "$margin_field" | sed 's/^\[meta_margin\]:.*MARGIN: //')
+style_field=$(grep '\[meta_style\]:.*STYLE:' "$1")
+[ -z "$style_field" ] && fatal "STYLE field must be present in MD file as a comment."
+STYLE=$(echo "$style_field" | sed 's/^\[meta_style\]:.*STYLE: //')
 
 [ -z "$TITLE" ] && fatal "TITLE variable must be set"
 [ -z "$AUTHOR" ] && fatal "AUTHOR variable must be set"
 [ -z "$DOC" ] && fatal "DOC variable must be set"
-[ -z "$LANGUAGE" ] && fatal "LANGUAGE variable must be set"
-[ -z "$MARGIN" ] && fatal "MARGIN variable must be set"
+[ -z "$STYLE" ] && fatal "STYLE variable must be set"
 
 DT=$(date '+%d %b %Y')
 DFN=$(date '+%d_%b_%Y')
@@ -43,6 +38,6 @@ sed -i 's!^---pagebreak[ \t]*$!```{=openxml}\n<w:p><w:r><w:br w:type="page"/></w
 # pandoc -f markdown -t odt ${doc} -M "title=$TITLE" -o ${DOC}.odt --reference-doc=./odt_gost_TEMPLATE.odt
 
 # DOCX version:
-pandoc -f markdown -t docx ${doc} -M "title=$TITLE" -o ${DOC}.docx --reference-doc=./docx_gost_TEMPLATE.docx
+pandoc -f markdown -t docx ${doc} -M "title=$TITLE" -o ${DOC}.docx --reference-doc=./docx_${STYLE}_TEMPLATE.docx
 
 rm -f $doc
--- render_to_pdf.sh
+++ render_to_pdf.sh
@@ -16,19 +16,14 @@ author_field=$(grep '\[meta_author\]:.*AUTHOR:' "$1")
 [ -z "$author_field" ] && fatal "AUTHOR field must be present in MD file as a comment."
 AUTHOR=$(echo "$author_field" | sed 's/^\[meta_author\]:.*AUTHOR: //')
 
-lang_field=$(grep '\[meta_language\]:.*LANGUAGE:' "$1")
-[ -z "$lang_field" ] && fatal "LANGUAGE field must be present in MD file as a comment."
-LANGUAGE=$(echo "$lang_field" | sed 's/^\[meta_language\]:.*LANGUAGE: //')
-
-margin_field=$(grep '\[meta_margin\]:.*MARGIN:' "$1")
-[ -z "$margin_field" ] && fatal "MARGIN field must be present in MD file as a comment."
-MARGIN=$(echo "$margin_field" | sed 's/^\[meta_margin\]:.*MARGIN: //')
+style_field=$(grep '\[meta_style\]:.*STYLE:' "$1")
+[ -z "$style_field" ] && fatal "STYLE field must be present in MD file as a comment."
+STYLE=$(echo "$style_field" | sed 's/^\[meta_style\]:.*STYLE: //')
 
 [ -z "$TITLE" ] && fatal "TITLE variable must be set"
 [ -z "$AUTHOR" ] && fatal "AUTHOR variable must be set"
 [ -z "$DOC" ] && fatal "DOC variable must be set"
-[ -z "$LANGUAGE" ] && fatal "LANGUAGE variable must be set"
-[ -z "$MARGIN" ] && fatal "MARGIN variable must be set"
+[ -z "$STYLE" ] && fatal "STYLE variable must be set"
 
 DT=$(date '+%d %b %Y')
 DFN=$(date '+%d_%b_%Y')
@@ -37,5 +32,5 @@ doc=$(mktemp ${DOC}_XXXXXX.md)
 cat ${DOC}.md | sed 's/^---$/\&nbsp;\n\n---\n\n\&nbsp;/' > $doc
 sed -i 's!^---pagebreak[ \t]*$!<div style="page-break-after: always; visibility: hidden">\n\\pagebreak\n</div>\n!' $doc 
 sed -i 's!^```[a-z][a-z]*[ \t]*$!`\\smallskip`{=latex}\n&!' $doc
-pandoc --pdf-engine lualatex -H make-code-footnotesize.tex --include-in-header=nohyphen.cfg --highlight-style=highlight.theme -V geometry:margin=${MARGIN} -M "title=$TITLE" -M "author=$AUTHOR" -M "date=$DT" ${doc} style_${LANGUAGE}.yml -o ${DOC}_${DFN}.pdf
+pandoc --pdf-engine lualatex -H make-code-footnotesize.tex --include-in-header=nohyphen.cfg --highlight-style=highlight.theme -M "title=$TITLE" -M "author=$AUTHOR" -M "date=$DT" ${doc} style_${STYLE}.yml -o ${DOC}_${DFN}.pdf
 rm -f $doc
